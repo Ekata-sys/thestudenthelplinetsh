34852e032689f3af469e59e53ed1a896d0357f43
Ekata-sys <68047382+Ekata-sys@users.noreply.github.com>
2023-09-23 09:36:05 +0530

    ui changes

--- src/Components/AU/Hero.jsx
+++ src/Components/AU/Hero.jsx
@@ -37,7 +37,7 @@ function Hero() {
                <div className="col-lg-5">
                   <div>
                      {/* <img src="assets/img/hero/hero-img-1.png" alt="hero" /> */}
-                     <div className='form d-flex justify-content-center pb-5 w-74 ps-0 ps-lg-5 ms-0 ms-lg-5'>
+                     <div className="formback">
                 <Form/>
                 <br/>
               </div>
